da424059a1e67b8e5d8fd7bcf07095b2eea39e64
sophiexinyue <sophiepan2004@gmail.com>
2025-05-20 18:15:15 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+import re
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
